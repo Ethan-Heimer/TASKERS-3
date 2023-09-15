2d5570ead843bd045d51100f871735bc7139e8dc
Ethan-Heimer <111470266+Ethan-Heimer@users.noreply.github.com>
2023-09-15 13:10:01 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const app = express();
+
+const bodyParser = require('body-parser');
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.set('view engine', "ejs");
+app.set('views', "./views")
+
+let tasks = []; 
+
+app.get("/", (req, res) => {
+    res.render("index", {tasks});
+})
+
+app.post("/addTask", (req, res) => {
+    const newTask = req.body.task;
+    console.log(newTask);
+
+    tasks.push({id: Date.now(), text: newTask});
+   
+    res.redirect("/");
+})
+
+app.get('/edit/:id', (req, res) => {
+    const taskId = parseInt(req.params.id);
+    const task = tasks.find(task => task.id === taskId);
+    res.render('edit', {task});
+});
+
+app.post('/edit/:id', (req, res) => {
+    const taskId = parseInt(req.params.id);
+    const updatedText = req.body.task;
+    const task = task.find(task => task.id === taskId);
+    if (task)
+        task.text = updatedText;
+        res.redirect('/');  
+})
+
+
+
+app.listen(3000, () => {});
